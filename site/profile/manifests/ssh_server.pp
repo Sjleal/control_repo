fa4dde819850e329c847efc999bbd026aa62f79c
Sjleal <49486078+Sjleal@users.noreply.github.com>
2019-11-12 00:32:50 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDPaLG/Lp1cA86vaBWU/mr3704NeYOR/qHFqow8QkmlF9pVLKrPnebobpc0aEZNcKryvvDMWGke4FKMGLQ1Rd98PqjSqY0wZlYjt/G4JBvJBsvJg6IlWU5NvXSKXNq3Gclu3OidzCUbY/34ZhieOX5cmY3cRpwe6z4pCWzarpWz5vZtkXr8lZYf6WACs1qLKFYVs7a92JMMeqVC7UZdagguWnHUZXiCO1i1pNnquzZl91Rko+ScPP+1VY47bZM0CqWEdXYCTEDDPblWqzty+odmRUL6vxYXFYGDKSbl+ESTN+ySHk0f32qe20epNFeADee3s3vxC5Px4Llo/4ajYZXh',
 	}  
 }
